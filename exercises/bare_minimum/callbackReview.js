7b8f3bbc0ac973d83635e73c5557f26bb256d0b8
thebantz <nicoledbanta@gmail.com>
2020-02-24 20:22:42 -0800

    Finish callbackReview

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -5,14 +5,34 @@
 var fs = require('fs');
 var request = require('request');
 
+var CustomError = function(code, message) {
+  this.code = code;
+  this.message = message;
+};
+
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      callback(new CustomError('ENOENT', 'Could not read file'));
+    } else {
+      data = data.toString().split('\n');
+      data.length = 1;
+      callback(null, data.toString());
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request.get(url, (err, response) => {
+    if (err) {
+      callback(new Error('Invalid URI'));
+    } else {
+      callback(null, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
